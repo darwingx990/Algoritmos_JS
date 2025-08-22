e98eab1103f72655e8aec7c44f3bb8d441a84016
Darwing <darwingd.mosqueraa@ecci.edu.co>
2025-08-21 20:27:25 -0500

    Exercises #1 and 2 for ciclosParte1 done

---
+++ ciclosParte1.js
@@ -0,0 +1,35 @@
+// 1. Algoritmo para imprimir números del 1 al 10
+console.log("---- Numeros del 1 al 10 ----");
+for (let i = 1; i < 11; i++) {
+    console.log("           Numero " + i);
+}
+
+// 2. Algoritmo para sumar los primeros 10 números
+let numbers = 0
+for (let i = 0; i <= 10; i++) {
+    numbers = numbers + i;
+}
+console.log("La suma de los numeros del 1 al 10 es igual a: " + numbers);
+
+// 3. Algoritmo para generar la tabla de un numero dado por argumento en una función
+function tablaDeMultiplicar(number) {
+    console.log("---------------------------------------------");
+    console.log("     Tabla de multiplicar del: " + number);
+    console.log("---------------------------------------------");
+}
+
+// 4. Algoritmo para contar ocurrencias de 'a' en un texto
+
+// 5. Algoritmo para calcular el factorial de un número
+
+// 6. Escribe una función que reciba un array de números y devuelva un nuevo array que contenga solo los números pares.
+
+// 7. Implementa una función que calcule la suma de los cuadrados de los primeros N números naturales.
+
+// 8. Escribe una función que calcule la potencia de un número (base^exponente) utilizando un ciclo for, sin usar el operador de potencia **.
+
+// 9. Desarrolla una función que genere y devuelva los primeros N términos de la serie de Fibonacci.
+
+// 10. Desarrolla una función que genere el total de las tablas de multiplicar dado un numero entero.
+
+q  
