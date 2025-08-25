df58618a6ccc99331849a2578fcea73ba26cba67
Darwing <darwingx990@hotmail.com>
2025-08-25 14:12:08 -0500

    CiclosParte1, exercises #3 and 4, done and tested.

--- ciclosParte1.js
+++ ciclosParte1.js
@@ -14,11 +14,23 @@ console.log("La suma de los numeros del 1 al 10 es igual a: " + numbers);
 // 3. Algoritmo para generar la tabla de un numero dado por argumento en una función
 function tablaDeMultiplicar(number) {
     console.log("---------------------------------------------");
-    console.log("     Tabla de multiplicar del: " + number);
+    console.log("     Tabla de multiplicar del " + number);
     console.log("---------------------------------------------");
+    for (let i = 1; i <= 10; i++) {
+        console.log("           " + number + " X " + i + " = " + number * i);
+    }
 }
+tablaDeMultiplicar(5);
 
 // 4. Algoritmo para contar ocurrencias de 'a' en un texto
+let cantidadDeAs = 0;
+let texto = "Mi mamá me mima. Mi mamá me ama. Amo a mamá"
+for (let i = 0; i <= texto.length; i++) {
+    if (texto[i] == "a" || texto[i] == "A" || texto[i] == "á") {
+        cantidadDeAs++;
+    }
+}
+console.log("El texto '" + texto + "', tiene un total de " + cantidadDeAs + " letras 'a'.");
 
 // 5. Algoritmo para calcular el factorial de un número
 
@@ -31,5 +43,3 @@ function tablaDeMultiplicar(number) {
 // 9. Desarrolla una función que genere y devuelva los primeros N términos de la serie de Fibonacci.
 
 // 10. Desarrolla una función que genere el total de las tablas de multiplicar dado un numero entero.
-
-q  
